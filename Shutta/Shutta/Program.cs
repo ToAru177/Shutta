39e255f8f319425d28df718901636ffa982d1c87
ToAru177 <49936454+ToAru177@users.noreply.github.com>
2019-05-16 15:57:18 +0900

    시작, 오링

--- Shutta/Shutta/Program.cs
+++ Shutta/Shutta/Program.cs
@@ -14,15 +14,31 @@ namespace Shutta
 
         static void Main(string[] args)
         {
+
+            Console.WriteLine("플레이어 수를 입력하세요. (2 ~ 5명)");
+            int numOfPlayer = int.Parse(Console.ReadLine());
+            // 2보다 작거나, 5보다 큰수 입력시 재입력 요구
+
+            Console.WriteLine("기본 소지금을 입력하세요.(500 ~ 2000)");
+            int seedMoney = int.Parse(Console.ReadLine());
+            // 금액 범위 위반시 재입력 요구
+
+            Console.WriteLine("기본 판돈을 입력하세요.(100 ~ 500)");
+            int battingMoney = int.Parse(Console.ReadLine());
+            // 금액 범위 위반시 재입력 요구
+
             Console.WriteLine("룰 타입을 선택하세요. (1:Basic, 2:Simple)");
             int input = int.Parse(Console.ReadLine());
+            // 1,2 외에 수 입력시 재입력 요구
+
+
 
             RuleType ruleType = (RuleType)input;
 
 
             // 두명의 플레이어 존재
             List<Player> players = new List<Player>();
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < numOfPlayer; i++)
             {
                 if (ruleType == RuleType.Basic)
                     players.Add(new BasicPlayer());
@@ -30,9 +46,10 @@ namespace Shutta
                     players.Add(new SimplePlayer());
             }
 
+
             // player들에게 기본금 지급
-            foreach(var player in players)
-                player.Money = SeedMoney;
+            foreach (var player in players)
+                player.Money = seedMoney;
 
             int round = 1;
 
@@ -40,7 +57,10 @@ namespace Shutta
             {
                 // 한명이 오링되면 게임 종료
                 if (IsAnyoneOring(players))
+                {
+                    MoneySort(players);
                     break;
+                }
 
                 Console.WriteLine($"============= Round {round++} =============");
 
@@ -49,8 +69,8 @@ namespace Shutta
                 // 학교 출석
                 foreach (var player in players)
                 {
-                    player.Money -= BattingMoney;
-                    dealer.PutMoney(BattingMoney);
+                    player.Money -= battingMoney;
+                    dealer.PutMoney(battingMoney);
                 }
 
                 // 카드 돌리기
@@ -68,6 +88,7 @@ namespace Shutta
                 Console.WriteLine("-----------------------------------");
 
 
+
                 // 승자 찾기
                 Player winner = FindWinner(players);
 
@@ -75,21 +96,36 @@ namespace Shutta
                 winner.Money += dealer.GetMoney();
 
                 // 각 플레이어의 돈 출력
-                
+
                 foreach (var player in players)
                     Console.Write("\t" + player.Money + "\t");
                 Console.WriteLine();
                 Console.WriteLine("===================================");
             }
+            int cnt = 1;
+            foreach (var player in players)
+            {
+                Console.WriteLine((cnt++) + " " + player.Money);
+            }
 
+        }
+
+        private static void MoneySort(List<Player> players)
+        {
+            players.Sort((Player p1, Player p2) => p2.Money.CompareTo(p1.Money));
         }
 
         private static Player FindWinner(List<Player> players)
         {
+            List<int> score = new List<int>();
+            for (int i = 0; i < players.Count(); i++)
+                score.Add(players[i].CalculateScore());
+            /*
             int score0 = players[0].CalculateScore();
             int score1 = players[1].CalculateScore();
+            */
 
-            if (score0 > score1)
+            if (score[0] > score[1])
                 return players[0];
             // 무승부인 경우 추가하기
             // 학교를 한번더 가기
@@ -99,7 +135,7 @@ namespace Shutta
 
         private static bool IsAnyoneOring(List<Player> players)
         {
-            foreach(Player player in players)
+            foreach (Player player in players)
                 if (player.Money == 0)
                     return true;
             return false;
