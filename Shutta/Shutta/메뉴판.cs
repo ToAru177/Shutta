114eec6420da8ff9cbf207d477abac5eebaed0a6
Rosebud326 <key2091@naver.com>
2019-05-17 12:10:15 +0900

    00

---
+++ Shutta/Shutta/메뉴판.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace 메뉴판
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("/////////////////////////////////////////////////////////////////////////////");
+            Console.WriteLine("//\t\t\t\t\t\t\t\t\t   //");
+            Console.WriteLine("//\t\t\t    ☆C#을 이용한 섯다☆\t\t\t   //");
+            Console.WriteLine("//\t\t\t\t\t\t\t\t\t   //");
+            Console.WriteLine("//\t1.게임 시작: 플레이어 수, 기본 소지금, 판돈, 룰타입 선택\t   //");
+            Console.WriteLine("//\t2.무승부의 경우: 추가 판돈 없이 카드 재배부\t\t\t   //");
+            Console.WriteLine("//\t3.오링의 경우: 한 명 오링시 게임종료 후 순위출력\t\t   //");
+            Console.WriteLine("//\t4.무승부의 경우: 추가 판돈 없이 카드 재배부\t\t\t   //");
+            Console.WriteLine("//\t5.배팅의 경우: 플레이어가 선택하여 배팅 가능\t\t\t   //");
+            Console.WriteLine("//\t6.무승부의 경우: 추가 판돈 없이 카드 재배부\t\t\t   //");
+            Console.WriteLine("//\t7.콜/다이의 선택: 패를 받고 콜 or 다이 선택 가능\t\t   //");
+            Console.WriteLine("//\t\t\t\t\t\t\t\t\t   //");
+            Console.WriteLine("/////////////////////////////////////////////////////////////////////////////");
+
+        }
+    }
+}
